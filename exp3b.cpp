06276c1c0a082ccde199e9745cf3fdee38ec97b3
nikhilraina99 <rainanikhilr@gmail.com>
2024-08-23 00:53:39 +0530

    Add files via upload

---
+++ exp3b.cpp
@@ -0,0 +1,13 @@
+//Nikhil
+//23070123093
+//entc b1
+//experiment 3b
+#include<iostream>
+using namespace std;
+int main(){
+    bool a= true;
+    bool b= true;
+    cout<<boolalpha;
+    cout<<"A&&B: "<<(a&&b)<<endl;
+    return 0;
+}
